efe80fbe450e9584d186ca5b7d296af561d8efbc
hardboyvino <97181625+hardboyvino@users.noreply.github.com>
2022-05-14 21:57:58 +0100

    Used Black and SonarLint to ensure code looks good

--- hackerrank_1_week_preparation_kit/day_1/Mini_Max.py
+++ hackerrank_1_week_preparation_kit/day_1/Mini_Max.py
@@ -14,23 +14,25 @@ from itertools import combinations
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
+
 def miniMaxSum(arr):
     # Get an empty list for all the sums
     sums = []
-    
+
     # Iterate over the array and get a combo of 4 numbers and sum them up then append to the sum to the sums array
     # This required importing combinations from the itertools module
     for number in combinations(arr, 4):
         sums.append(sum(number))
-    
+
     # Sort the sums array in ascending order
     sums = sorted(sums)
 
-    # Print the first sum and last sum separated by a space, not a new line    
+    # Print the first sum and last sum separated by a space, not a new line
     print(sums[0], end=" ")
     print(sums[-1])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     arr = list(map(int, input().rstrip().split()))
 
